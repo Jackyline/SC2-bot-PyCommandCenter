a111acb0667dd0782a978d6e7825d59acd0ab60a
dylma900 <dylma900@student.liu.se>
2019-11-05 15:46:51 +0100

    ResourceManager implemented in main, functions seems to work

--- classes/ResourceManager.py
+++ classes/ResourceManager.py
@@ -1,53 +1,60 @@
 #TODO
 #1. Test everything
-
+from library import *
 
 class ResourceManager():
-    def __init__(self, minerals, gas, supply, bot: IDAbot):
-        # Resources are saved with id: [minerals, gas, supply]
-        self.minerals = minerals
-        self.gas = gas
-        self.supply = supply
+    def __init__(self, minerals, gas, supply, bot: IDABot):
+        self.resources = Resources(minerals, gas, supply)
         self.bot = bot
-        self.current_reservations = {}
-        self.reserved = Reservation(0, 0, 0)
+        # Reservations have an id as key, and resources used as value (Resources object)
+        self.reservations = {}
+        # Total reserved resources
+        self.reserved = Resources(0, 0, 0)
 
-    def sync_resources(self):
-        self.minerals = self.bot.minerals
-        self.gas = self.bot.gas
-        self.supply = self.bot.supply
+    def sync(self):
+        self.resources.minerals = self.bot.minerals
+        self.resources.gas = self.bot.gas
+        self.resources.supply = self.bot.current_supply
 
     def get_minerals(self):
-        return self.bot.minerals - self.reserved.minerals
+        return self.resources.minerals - self.reserved.minerals
 
     def get_gas(self):
-        return self.bot.gas - self.reserved.gas
+        return self.resources.gas - self.reserved.gas
 
     def get_supply(self):
-        return self.bot.supply - self.reserved.supply
+        return self.resources.supply - self.reserved.supply
 
-    def reserve_resources(self, unit_type: UnitType, id):
-        minerals_reserved = unit_type.mineral_price
-        gas_reserved = unit_type.gas_price
-        supply_reserved = unit_type.supply_required
-        self.current_reservations[id] = Reservation(minerals_reserved, gas_reserved, supply_reserved)
+    def reserve(self, id, unit_type: UnitType):
+        minerals = unit_type.mineral_price
+        gas = unit_type.gas_price
+        supply = unit_type.supply_required
 
-    def release_reservation(self, id):
-        del self.current_reservations[id]
+        self.reserved.minerals += minerals
+        self.reserved.gas += gas
+        self.reserved.supply += supply
 
-    def use_resources(self, unit_type: UnitType):
-        self.minerals -= unit_type.mineral_price
-        self.gas -= unit_type.gas_price
-        self.supply -= unit_type.supply
+        self.reservations[id] = Resources(minerals, gas, supply)
+
+    def release(self, id):
+        self.reserved.minerals -= self.reservations[id].minerals
+        self.reserved.gas -= self.reservations[id].gas
+        self.reserved.supply -= self.reservations[id].supply
+        del self.reservations[id]
+
+    def use(self, unit_type: UnitType):
+        self.resources.minerals -= unit_type.mineral_price
+        self.resources.gas -= unit_type.gas_price
+        self.resources.supply -= unit_type.supply_required
 
     def can_afford(self, unit_type: UnitType):
         """ Returns True if there are an sufficient amount of minerals, gas and supply to build the given unit_type,
         alse otherwise """
         return self.get_minerals >= unit_type.mineral_price and \
                self.get_gas >= unit_type.gas_price \
-               and (bot.max_supply - self.get_supply) >= unit_type.supply_required
+               and (self.bot.max_supply - self.get_supply) >= unit_type.supply_required
 
-class Reservation():
+class Resources():
     def __init__(self, minerals, gas, supply):
         self.minerals = minerals
         self.gas = gas
--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import os
 
 from typing import Optional
 from library import *
-from ./classes/ResourceManager import *
-
+from classes.ResourceManager import ResourceManager
 
 class MyAgent(IDABot):
     def __init__(self):
         IDABot.__init__(self)
+        self.resource_manager = ResourceManager(self.minerals, self.gas, self.current_supply, self)
 
     def on_game_start(self):
         IDABot.on_game_start(self)
 
     def on_step(self):
         IDABot.on_step(self)
-
+        self.resource_manager.sync()
 
 def main():
     coordinator = Coordinator(r"D:\StarCraft II\Versions\Base69232\SC2_x64.exe")
@@ -26,7 +26,7 @@ def main():
     # participant_2 = create_participants(Race.Terran, bot2)
     participant_2 = create_computer(Race.Random, Difficulty.Easy)
 
-    coordinator.set_real_time(True)
+    coordinator.set_real_time(False)
     coordinator.set_participants([participant_1, participant_2])
     coordinator.launch_starcraft()
 
