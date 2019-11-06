1778466253e42edb9b4893e7a758b8ae3b13b965
Anton Hansson <antha652@student.liu.se>
2019-11-06 14:59:50 +0100

    gjort lite skit

---
+++ classes/BuildingManager.py
@@ -0,0 +1,53 @@
+from library import *
+from classes.BuildingUnit import BuildingUnit
+from collections import defaultdict
+
+
+class BuildingManager:
+    def __init__(self, IDABot : IDABot):
+        self.IDABot = IDABot
+        self.buildings = []                         #:[BuildingUnit]
+        self.under_construction = []                      #:[BuildingUnit]
+
+
+    def on_step(self, buildings : [Unit]):
+        """ Update internal lists 'buildings' and 'under_construction'. """
+
+        # Check for completed buildings in self.under_construction
+        for b in self.under_construction:
+            if b.get_unit().is_completed:
+                self.under_construction.remove(b)
+                self.buildings.append(b)
+
+        #Add undiscovered buildings to self.buildings and self.under_construction
+        for building in buildings:
+            if not building in [b.get_unit() for b in self.buildings + self.under_construction]:
+                # Building is not in self.buildings, add it
+                building_unit = BuildingUnit(building)
+                if not building.is_completed:
+                    self.under_construction.append(building_unit)
+                else:
+                    self.buildings.append(building_unit)
+
+        # Remove buildings that no longer exists i.e. are destroyed
+        if len(buildings) != len(self.buildings) + len(self.under_construction):
+            self.buildings = [b for b in self.buildings if b.get_unit() in buildings]
+            self.under_construction = [b for b in self.under_construction if b.get_unit() in buildings]
+
+
+    def get_buildings_of_type(self, type : UnitType):
+        return [b for b in self.buildings if b.get_unit_type() == type]
+
+    def get_under_construction_of_type(self, type : UnitType):
+        return [b for b in self.under_construction if b.get_unit_type() ==  type]
+
+    def train(self, building : BuildingUnit, unit : Unit): # Todo: ändra till klasserna som vi själva gjort för unit
+        pass
+
+    def print_debug(self):
+        types_buildings = {}
+        for b in self.buildings:
+            types_buildings[b.get_unit_type_id()] = types_buildings.get(b.get_unit_type_id(), 0) +1
+        for key, value in types_buildings.items():
+            print(key, ": ", value)
+
--- classes/BuildingUnit.py
+++ classes/BuildingUnit.py
@@ -1,5 +1,33 @@
 from library import *
 
 class BuildingUnit:
-    def ___init___(self, unit : Unit):
+    """
+    Is one building.
+    Use methods defined here over methods/variables of Unit
+    """
+    def __init__(self, unit : Unit):
         self.unit = unit
+
+    def get_unit(self):
+        return self.unit
+
+    def get_unit_type_id(self):
+        return self.get_unit_type().unit_typeid
+
+    def get_unit_type(self):
+        return self.unit.unit_type
+
+    def get_hit_points(self):
+        return self.unit.hit_points
+
+    def is_alive(self):
+        return self.unit.is_alive
+
+    def is_training(self):
+        return self.unit.is_alive
+
+    def get_tile_pos(self):
+        return self.unit.tile_position
+
+    def get_pos(self):
+        return self.unit.position
