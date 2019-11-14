598115ae450122546931a13a606295f75113fcd5
Benjamin Hansson <benha109@student.liu.se>
2019-11-14 16:56:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,51 +9,31 @@ from classes.unit_manager import UnitManager
 class MyAgent(IDABot):
     def __init__(self):
         IDABot.__init__(self)
-
+        self.resource_manager = ResourceManager(self.minerals, self.gas, self.current_supply, self)
+        self.unit_manager = UnitManager(self)
 
     def on_game_start(self):
         IDABot.on_game_start(self)
 
-
     def on_step(self):
         IDABot.on_step(self)
-        units = self.get_all_units()
-        my_units = []
-        enemy_units = []
-        for unit in units:
-            if unit.unit_type.unit_typeid == UNIT_TYPEID.TERRAN_MARINE:
-                if unit.player == PLAYER_ENEMY:
-                    enemy_units.append(unit)
-                else:
-                    my_units.append(unit)
-
-        if len(enemy_units) > 0:
-            for my in my_units:
-                my.attack_unit(enemy_units[0])
-        else:
-            enemy_base = self.base_location_manager.get_player_starting_base_location(PLAYER_ENEMY)
-            for unit in my_units:
-                unit.move(enemy_base.position)
-
-
-
-
-
+        self.resource_manager.sync()
+        self.unit_manager.on_step(self.get_my_units())
 def main():
     coordinator = Coordinator(r"D:\StarCraft II\Versions\Base69232\SC2_x64.exe")
 
     bot1 = MyAgent()
-    bot2 = MyAgent()
+    # bot2 = MyAgent()
 
     participant_1 = create_participants(Race.Terran, bot1)
-    participant_2 = create_participants(Race.Terran, bot2)
-    #participant_2 = create_computer(Race.Terran, Difficulty.Easy)
+    # participant_2 = create_participants(Race.Terran, bot2)
+    participant_2 = create_computer(Race.Random, Difficulty.Easy)
 
     coordinator.set_real_time(False)
     coordinator.set_participants([participant_1, participant_2])
     coordinator.launch_starcraft()
 
-    path = os.path.join(os.getcwd(), "maps","test6.SC2Map")
+    path = os.path.join(os.getcwd(), "maps", "InterloperTest.SC2Map")
     coordinator.start_game(path)
 
     while coordinator.update():
