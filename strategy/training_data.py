83c12fe4b272baa22aa1bee5cdc862d09b1cd7eb
Hugo Cedervall <hugce564@student.liu.se>
2019-11-20 10:24:42 +0100

    Can now extract worker/armie count from replays at given time

--- strategy/training_data.py
+++ strategy/training_data.py
@@ -1,4 +1,5 @@
 import pickle
+import mpyq
 
 
 class MatchStates:
@@ -22,9 +23,10 @@ class MatchStates:
 class MatchState:
     def __init__(self):
         # Observed game state
-        self.resources = None  # {low, mid, high}
-        self.expansions = None  # {0, ..., 16}
-        self.armies = None  # [0,1]
+        self.minerals = None  # [0, ..., n]
+        self.gas = None         # [0, .., n]
+        self.expansions = None  # [0, ..., n]
+        self.armies = None  # [0, ..., n]
         self.defensive_buildings = None  # [0,1]
         self.production_buildings = None  # [0,1]
 
@@ -58,4 +60,127 @@ def get_states(filename):
         return pickle.load(file)
 
 
-he = MatchStates("hejsan.txt")
+# he = MatchStates("hejsan.txt")
+
+
+# Extract data from replays
+
+from s2protocol import versions
+
+
+def open_replay(replay_name):
+    # Open replay
+    archive = mpyq.MPQArchive(
+        '/home/hugo/LIU/tddd92-projekt-2019-storgrupp-2-01/strategy/replays_p3/{filename}'.format(filename=replay_name))
+
+    print(archive.files)
+
+    contents = archive.header['user_data_header']['content']
+    header = versions.latest().decode_replay_header(contents)
+    baseBuild = header['m_version']['m_baseBuild']
+    protocol = versions.build(baseBuild)
+
+    contents = archive.read_file('replay.game.events')
+    gameEvents = protocol.decode_replay_game_events(contents)
+
+    print(gameEvents)
+
+    for event in gameEvents:
+        print(event)
+
+
+#open_replay('Clem_v_Ziggy:_Game_2_-_Kairos_Junction_LE.SC2Replay')
+
+import sc2reader
+
+
+def worker_counter(replay, second, player_id):
+    workers = []
+    for event in replay.events:
+        if event.name == "UnitBornEvent" and event.control_pid == player_id:
+            if event.unit.is_worker:
+                workers.append(event.unit)
+
+        if event.name == "UnitDiedEvent":
+            if event.unit in workers:
+                workers.remove(event.unit)
+
+        if event.second > second:
+            break
+
+    return len(workers)
+
+def army_counter(replay, second, player_id):
+    armie = []
+    for event in replay.events:
+        if event.name == "UnitBornEvent" and event.control_pid == player_id:
+            if event.unit.is_army:
+                armie.append(event.unit)
+                print(event.unit.name)
+
+        if event.name == "UnitDiedEvent":
+            if event.unit in armie:
+                armie.remove(event.unit)
+
+        if event.second > second:
+            break
+
+    return len(armie)
+
+def building_counter(replay, second, player_id):
+    buildings = []
+    for event in replay.events:
+        if event.name == "UnitBornEvent" and event.control_pid == player_id:
+            if event.unit.is_building:
+                buildings.append(event.unit)
+                print(event.unit.name)
+
+        if event.name == "UnitDiedEvent":
+            if event.unit in buildings:
+                buildings.remove(event.unit)
+
+        if event.second > second:
+            break
+
+    return len(buildings)
+
+
+
+def open_replay2(replay_name):
+    replay = sc2reader.load_replay(
+        '/home/hugo/LIU/tddd92-projekt-2019-storgrupp-2-01/strategy/replays_p3/{filename}'.format(filename=replay_name),
+        load_map=True)
+    print(replay)
+    print(type(replay))
+
+    event_names = set([event.name for event in replay.events])
+    print(event_names)
+
+    events_of_type = {name: [] for name in event_names}
+    for event in replay.events:
+        events_of_type[event.name].append(event)
+
+    for event in events_of_type:
+        print(event)
+
+    #for elem in replay.events:
+    #    print(elem)
+
+    print("Workers")
+    print(worker_counter(replay, 400, 1))
+    print(worker_counter(replay, 400, 2))
+
+    print("Armies")
+    print(army_counter(replay, 400, 1))
+    print(army_counter(replay, 400, 2))
+
+    print("Buildings")
+    print(building_counter(replay, 600, 1))
+    print(building_counter(replay, 600, 2))
+
+    length_of_game = replay.frames // 24
+
+    print(length_of_game)
+
+
+open_replay2('Clem_v_Ziggy:_Game_2_-_Kairos_Junction_LE.SC2Replay')
