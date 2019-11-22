b1a789f5a1828ba3e8edd69be56eedabb4bcda36
Viktor Hellqvist <vikhe931@student.liu.se>
2019-11-22 13:15:58 +0100

    csg works, very inefficent

--- classes/coalitionstructure_generation.py
+++ classes/coalitionstructure_generation.py
@@ -1,4 +1,6 @@
 #from library import *
+from typing import Dict, Any
+
 
 class CoalitionstructureGeneration:
 
@@ -39,10 +41,14 @@ class CoalitionstructureGeneration:
         if(sum(coalition) == 0):
             return 0
 
+        # Generate all possible list where every element <= equivalent coalition element
+        # Remove last as we dont want [0, 0, ..., 0] list
         all_b = []
         self.generate_all_b(coalition, all_b)
+        all_b = all_b[:-1]
 
-        maxValue = self.get_v(coalition)
+        max_value = self.get_v(coalition)
+        max_coal = [coalition]
         for set_of_b in all_b:
             # Create new coalition [a1-b1, a2-b2, ...]
             new_col = []
@@ -50,17 +56,18 @@ class CoalitionstructureGeneration:
                 new_col.append(coalition[i] - set_of_b[i])
 
             value = self.get_q(new_col) + self.get_v(set_of_b)
-            if value > maxValue:
-                maxValue = value
+            if value > max_value:
+                max_value = value
+                max_coal = self.get_r(new_col) + [set_of_b] #this is basically optimal structure for new_col + set of b
 
-        return maxValue
+        self.set_r(coalition, max_coal)
+        return max_value
 
     def set_q(self, coalition, value):
         if self.coalition_str(coalition) in self.q_dict:
             if value < self.q_dict[self.coalition_str(coalition)]:
                 return
         self.q_dict[self.coalition_str(coalition)] = value
-
 
     def get_q(self, coalition):
         """
@@ -75,9 +82,25 @@ class CoalitionstructureGeneration:
             self.q_dict[self.coalition_str(coalition)] = f_value
             return f_value
 
+    def set_r(self, coalition, optimal_cs):
+        self.r_dict[self.coalition_str(coalition)] = optimal_cs
+
     def get_r(self, coalition):
-        if self.coalition_str(coalition) in self.v_dict:
-            return self.v_dict[self.coalition_str(coalition)]
+        if self.coalition_str(coalition) in self.r_dict:
+            coal_str = self.coalition_str(coalition)
+            if self.r_dict[coal_str] == coalition:
+                return coalition
+            else:
+                # input coalition's optimal structure is not just the input coalition so we do:
+                # get the optimal structure for the coalition in self.r_dict[coalition] which is the a-b values in definition of f
+                # we combine those lists with the optimal structure for coalition - r_dict[coalition] which is the b values in definition of f
+
+                # For every typ of agent, sum all agents of a type in the coalition structure and save in one list
+                # e.g. structure [ [1, 2, 2], [1, 3, 5]] would become [2, 5, 7]
+                #known_struct = [ sum([temp_coal[agent_type] for temp_coal in self.r_dict[coal_str]]) for agent_type in range(len(coalition))]
+                #optiaml_structure = self.r_dict[coal_str] + \
+                #       self.get_r([original_coal - optimal_coal for original_coal, optimal_coal in zip(coalition, known_struct)])
+                return self.r_dict[coal_str]
         else:
             return None
 
@@ -87,10 +110,19 @@ class CoalitionstructureGeneration:
         :param coalition: coalition type to be evaluated
         :return: int
         """
-        # TODO
-        if any([group == 0 for group in coalition]):
+        # TODO: Testa v med minsta koalition för olika antal av varje typ t.ex. [5, 3, 3]
+        #  bästa borde vara [1, 1, 1], [2, 1, 1], [2, 1, 1]?
+        #"""
+        if any([nr_of_agents == 0 for nr_of_agents in coalition]):
             return 0
         value = 1 / sum(map(lambda x: 1 + x**2, coalition))
+        """
+        value = 0
+        if coalition.count(0) == 2:
+            return sum(map(lambda x: x**2, coalition))
+        else:
+            return 0
+        """
         return value
 
     def get_v(self, coalition):
@@ -126,7 +158,6 @@ class CoalitionstructureGeneration:
         # Remove last , and return
         return a[:-1]
 
-
     def generate_all_b(self, coalition, all_b, index = 0):
         """
         :param coalition: coalition type
@@ -144,7 +175,12 @@ class CoalitionstructureGeneration:
         coalition[index] = start_value
 
 csg = CoalitionstructureGeneration()
+list_to_test = [5, 5, 3]
 b = []
-csg.generate_all_b([2,1,12], b)
-a = csg.get_q([2, 2, 2])
-print(a)
+a = csg.get_q(list_to_test)
+cs = csg.get_r(list_to_test)
+print("optimal structure for {} has a value {} and is splitted as:".format(list_to_test, a))
+output = ""
+for coalition in cs:
+    output += "({}, value: {}), ".format(coalition, csg.q_dict[csg.coalition_str(coalition)])
+print(output[:-2])
