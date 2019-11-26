2d06219d2fd419f959fbbd3d1ae223d36185e397
Anton <antha652@student.liu.se>
2019-11-26 13:28:23 +0100

    changes task_manager

--- classes/task_manager.py
+++ classes/task_manager.py
@@ -3,20 +3,71 @@ import numpy as np
 from munkres import print_matrix
 #import unit_manager
 
-class TaskManager:
+class WorkerAssignments():
+    def __init__(self):
+        self.assignments = {}
+
+    def utility_func(self, worker, task):
+        return 3
+
+    def get_all_units(self):
+        pass
+
+    def toString(self):
+        return "worker assignments"
+
+    def update(self, new_assignments : dict):
+        self.assignments.update(new_assignments)
+
+class MilitaryAssignments():
+    def __init__(self):
+        self.assignments = {}
+
+    def utility_func(self, group, task):
+        return 5
+
+    def get_all_units(self):
+        pass
+
+    def toString(self):
+        return "military assignments"
+
+    def update(self, new_assignments : dict):
+        self.assignments.update(new_assignments)
+
+class BuildingAssignments():
+    def __init__(self):
+        self.assignments = {}
+
+    def utility_func(self, building, task):
+        return 10
+
+    def get_all_units(self):
+        pass
+
+    def toString(self):
+        return "building assignments"
+
+    def update(self, new_assignments : dict):
+        """
+        for key, value in new_assignments:
+
+        for key, value in self.assignments:
+            if value in
+
+        """
+        self.assignments.update(new_assignments)
+
+class TaskManager():
 
     #    def __init__(self, IDABot : IDABot):
             #self.idabot = IDABot
 
     def __init__(self):
+        self.worker_assignments = WorkerAssignments()
+        self.military_assignments = MilitaryAssignments()
+        self.building_assignments = BuildingAssignments()
         self.hungarian = hungarian.Hungarian()
-        self.military_tasks = []
-        self.workers_tasks = []
-        self.building_tasks = []
-        self.assignments = {}
-
-    def add_task(task):
-        pass
 
     def generate_matrix(self, utility_func, units, tasks):
         """
@@ -55,32 +106,42 @@ class TaskManager:
     def get_worker_tasks():
         return self.worker_tasks
 
-    def svc_utility_func(self, unit, task):
-        return 5
-
-    def military_utility_func(self, groups, task):
-        return 10
-
-    def calc_assignments(self, utility_func, units, tasks):
-        matrix = self.generate_matrix(utility_func, units, tasks)
-        print_matrix(matrix, msg="\nGenerated svc matrix")
+    def calc_assignments(self, task_type, tasks):
+        #units = task_type.get_all_units()
+        units = ["anton", "benjamin", "hugo", "viktor", "hankish", "dylan", "fredrik", "mattias", "björn"] # instead of get all units
+        matrix = self.generate_matrix(task_type.utility_func, units, tasks)
+        print_matrix(matrix, msg="Matrix generated for: " + task_type.toString())
         matching = self.hungarian.compute_assignments(matrix)
         self.hungarian.pretty_print_assignments()
         assignments = self.convert_matching_to_assignments(matching, units, tasks)
-        print(assignments)
+        return assignments
 
-    def update_assignments(self, new_assignments : dict):
-        self.assignments.update(new_assignments)
+        task_type.utility_func
+
+    def update_assignments(self, task_type, tasks : list):
+        print("\n" + task_type.toString() + " before update", task_type.assignments, "\n")
+        assignments = self.calc_assignments(task_type, tasks)
+        task_type.update(assignments)
+        print(task_type.toString() + " after update", task_type.assignments)
+
+    def on_step(self, new_tasks):
+        # Update worker assignments
+        self.update_assignments(self.worker_assignments, new_tasks)
+
+        # Update military assignments
+        self.update_assignments(self.military_assignments, new_tasks)
+
+        # Update building assignments
+        self.update_assignments(self.building_assignments, new_tasks)
+
+        pass
 
 def main():
-    units = ["anton", "benjamin", "hugo", "viktor", "hankish", "dylan", "fredrik", "mattias", "björn"]
     tasks = ["clean", "wash", "paint", "attack", "mine", "scout", "build"]
     tasks2 = ["clean", "wash", "paint", "attack", "mine", "scout", "build", "bajsa", "dricka", "dansa", "skriva", "heja"]
 
     task_manager = TaskManager()
-    task_manager.calc_assignments(task_manager.svc_utility_func, units, tasks)
-    task_manager.calc_assignments(task_manager.military_utility_func, units, tasks2)
-
+    task_manager.on_step(tasks)
 
 if __name__ == "__main__":
     main()
